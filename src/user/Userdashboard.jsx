b1e501dc062f8a23fe1f776d43bd4d9ef148e7f7
Simranjit911 <simranjit94938@gmail.com>
2023-09-09 23:01:49 +0530

    added userdash loader

--- src/user/Userdashboard.jsx
+++ src/user/Userdashboard.jsx
@@ -1,19 +1,44 @@
-import Usermenu from "../components/Usermenu"
-import {useAuth} from "../UserContext"
+import Usermenu from "../components/Usermenu";
+import { useAuth } from "../UserContext";
+import Loader from "../components/Loader";
+import { useState } from "react";
 const Userdashboard = () => {
-  const [userauth]=useAuth()
-  return (
-    <div className="flex flex-col  items-center w-full   h-[100vh]">
-      <Usermenu/>
-      <div className=" w-[80%] md:w-1/3  items-center text-gray-900 flex flex-col bg-gray-300 px-5 py-2 shadow-xl rounded-lg gap-4 text-lg text-slate-950 ">
-      <h1 className="text-xl font-bold">User Details:</h1>
-      <h2><span className="text-xl font-semibold">Name: </span>{userauth?.user?.name}</h2>
-      <h2><span  className="text-xl font-semibold">Email: </span>{userauth?.user?.email}</h2>
-      <h2><span  className="text-xl font-semibold">Phone: </span>{userauth?.user?.phone}</h2>
-      <h2><span  className="text-xl font-semibold">Address: </span>{userauth?.user?.address}</h2>
-      </div>
-    </div>
-  )
-}
+  const [userauth] = useAuth();
+  const [load, setload] = useState(true);
 
-export default Userdashboard
+  setTimeout(() => {
+    setload(false);
+  }, 1000);
+  return (
+    <>
+      {load ? (
+        <Loader />
+      ) : (
+        <div className="flex flex-col  items-center w-full   h-[100vh]">
+          <Usermenu />
+          <div className=" w-[80%] md:w-1/3  items-center text-gray-900 flex flex-col bg-gray-300 px-5 py-2 shadow-xl rounded-lg gap-4 text-lg text-slate-950 ">
+            <h1 className="text-xl font-bold">User Details:</h1>
+            <h2>
+              <span className="text-xl font-semibold">Name: </span>
+              {userauth?.user?.name}
+            </h2>
+            <h2>
+              <span className="text-xl font-semibold">Email: </span>
+              {userauth?.user?.email}
+            </h2>
+            <h2>
+              <span className="text-xl font-semibold">Phone: </span>
+              {userauth?.user?.phone}
+            </h2>
+            <h2>
+              <span className="text-xl font-semibold">Address: </span>
+              {userauth?.user?.address}
+            </h2>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Userdashboard;
